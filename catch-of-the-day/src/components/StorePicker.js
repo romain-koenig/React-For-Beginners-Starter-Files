829d92fe1227639e6de3fd41943ea4a2c049ad74
Romain KOENIG <romain.koenig@gmail.com>
2020-04-01 13:43:53 +0200

    Using Helper function

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {getFunName} from '../helpers';
 
 class StorePicker extends React.Component {
 
@@ -7,7 +8,7 @@ class StorePicker extends React.Component {
           
                 <form className="store-selector">
                     <h2>Please Enter a Store</h2>
-                    <input type="text" required placeholder="Store Name"/>
+                    <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
                     <button type="submit">Visit Store ➡</button>
                 </form>
 
