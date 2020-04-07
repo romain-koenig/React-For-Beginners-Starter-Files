999eff859721cb60ba72c43249b019a2e47a6a86
Romain KOENIG <romain.koenig@gmail.com>
2020-04-07 09:15:05 +0200

    Routingfonctionnel

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,31 +2,38 @@ import React from "react";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+    
     myInput = React.createRef();
 
-    //this is the only syntax that allows usinf "this" in a custom method
+    //this is the only syntax that allows using "this" in a custom method
     // otherwise, binding everything in constructor
+    // goToStore is a property, therefore it's binded
     goToStore = event => {
         // 1. Stop the form from submitting
         event.preventDefault();
         // 2. get the text from that input
-        console.log(this);
+        const storeName = this.myInput.current.value;
 
         // 3. Change the page to /store/whatever-they-entered 
+        this.props.history.push(`/store/${storeName}`);
+        
     };
+
+
     render() {
         return (
-            <form className="store-selector" onSubmit={this.goToStore}>
-                           <h2>Please Enter A Store</h2>
+            < form className="store-selector" onSubmit={this.goToStore} >
+                {/* this is a comment */}
+                <h2>Please Enter Your Store</h2>
                 <input
                     type="text"
                     ref={this.myInput}
-                    required 
+                    required
                     placeholder="Store Name"
                     defaultValue={getFunName()}
-                    />
+                />
                 <button type="submit">Visit Store →</button>
-            </form>
+            </form >
         );
     }
 }
